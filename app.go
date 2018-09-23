9bdc830849037d709d648c903fd19b952cc32cab
Mathieu <worming@hotmail.be>
2018-09-23 20:33:30 +0200

    minor update

--- app.go
+++ app.go
@@ -12,7 +12,9 @@ const DEFAULT_NUMBER_SIMULATION int = 10000
 func main() {
 	seedRandom()
 
-	err, defenders, expectedPercent := GetValuesFromArgs(os.Args)
+
+
+	err, defenders, expectedPercent := GetValuesFromArgs(os.Args[1:])
 	if err != nil {
 		fmt.Println("error while parsing arguments.\n this program will use default values")
 		fmt.Println("call '<appname> <number of defender> <expected winrate>' to use this app")
@@ -29,11 +31,11 @@ func GetValuesFromArgs(args []string) (error, int, float32) {
 	if len(args) < 3 {
 		return errors.New("missing argumets"), 0, 0
 	}
-	defenders, err := strconv.ParseInt(args[1], 10, 32)
+	defenders, err := strconv.ParseInt(args[0], 10, 32)
 	if err != nil {
 		return err, 0, 0
 	}
-	expectedPercent, err := strconv.ParseFloat(args[2], 32)
+	expectedPercent, err := strconv.ParseFloat(args[1], 32)
 	if err != nil {
 		return err, 0, 0
 	}
